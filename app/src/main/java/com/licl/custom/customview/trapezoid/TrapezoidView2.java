5143dc42fcc08970ca2412b6df77c35951ae4528
lichl <lichl@chineseall.com>
2017-11-24 15:40:39 +0800

    提交梯形的角标的形状View以第二个为准

--- app/src/main/java/com/licl/custom/customview/trapezoid/TrapezoidView2.java
+++ app/src/main/java/com/licl/custom/customview/trapezoid/TrapezoidView2.java
@@ -7,6 +7,7 @@ import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
 import android.graphics.Rect;
+import android.graphics.RectF;
 import android.graphics.Typeface;
 import android.support.annotation.Nullable;
 import android.text.TextUtils;
@@ -21,28 +22,26 @@ import com.licl.custom.customview.utils.AndroidUtils;
  * Created by licl on 2017/11/24.
  */
 
-public class TrapezoidView extends View {
+public class TrapezoidView2 extends View {
 
     private Paint mBgPaint;
     private Paint mTextPaint;
     private Context mContext;
     private Path mPath;
+    private Path mTextPath;
     private float traHeight;//梯形高
 
     private String titleName = "测试";
     private Rect mTextBounds;
     private Rect mBounds;
+    private RectF mTextPathBounds;
 
-    public TrapezoidView(Context context) {
+    public TrapezoidView2(Context context) {
         this(context, null);
     }
 
-    public TrapezoidView(Context context, @Nullable AttributeSet attrs) {
+    public TrapezoidView2(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
-        init(context, attrs);
-    }
-
-    private void init(Context context, @Nullable AttributeSet attrs) {
         mContext = context;
         TypedArray typedArray = context.obtainStyledAttributes(attrs, R.styleable.TrapezoidView);
         traHeight = typedArray.getDimension(R.styleable.TrapezoidView_trapHeight, AndroidUtils.dip2px(mContext, 20));
@@ -57,15 +56,17 @@ public class TrapezoidView extends View {
         mBgPaint.setStyle(Paint.Style.FILL);
 
         mTextPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        mTextPaint.setColor(Color.WHITE);
+        mTextPaint.setColor(Color.BLACK);
         mTextPaint.setAntiAlias(true);
         mTextPaint.setStyle(Paint.Style.STROKE);
         mTextPaint.setTextSize(AndroidUtils.dip2px(mContext, 14));
         mTextPaint.setTypeface(Typeface.DEFAULT);//字体样式Typeface.BOLD
 
         mPath = new Path();
+        mTextPath = new Path();
         mTextBounds = new Rect();
         mBounds = new Rect();
+        mTextPathBounds = new RectF();
     }
 
 
@@ -93,23 +94,50 @@ public class TrapezoidView extends View {
         invalidate();
     }
 
+    /**
+     * 绘制文字
+     * @param canvas
+     */
     @Override
     protected void onDraw(Canvas canvas) {
-        //绘制背景
-        mPath.moveTo(0, mBounds.height());
-        mPath.lineTo(traHeight, mBounds.height() - traHeight);
-        mPath.lineTo(mBounds.width() - traHeight, mBounds.height() - traHeight);
+
+        mPath.moveTo(0, 0);
+        mPath.lineTo((int) Math.sqrt(2) * traHeight, 0);
+        mPath.lineTo(mBounds.width(), mBounds.height() - (int) Math.sqrt(2) * traHeight);
         mPath.lineTo(mBounds.width(), mBounds.height());
-        mPath.lineTo(0, mBounds.height());
+        mPath.lineTo(0, 0);
         canvas.drawPath(mPath, mBgPaint);
 
-        //绘制文字
+
         if (!TextUtils.isEmpty(titleName)) {
             mTextPaint.getTextBounds(titleName, 0, titleName.length(), mTextBounds);
-            float leftTitleBounds = traHeight + (mBounds.width() - 2 * traHeight - mTextBounds.width()) / 2;
-            float topTitleBounds = mBounds.height() - traHeight + (traHeight) * 0.5f + mTextBounds.height() * 0.5f - mTextBounds.bottom;
+            float topTitleBounds = getTopTitleBounds();
+            float leftTitleBounds = (int) Math.sqrt(2) * traHeight - (int) Math.sqrt(0.5) * mTextBounds.height();
 
-            canvas.drawText(titleName, leftTitleBounds, topTitleBounds, mTextPaint);
+            drawText(canvas, titleName, leftTitleBounds, topTitleBounds, mTextPaint, 45);
+
+        }
+    }
+
+    private float getTopTitleBounds() {
+        int basH = (int) (traHeight - mTextBounds.height()) / 2;
+
+        int xBwidth = (int) Math.sqrt(Math.pow(mBounds.width(), 2) + Math.pow(mBounds.height(), 2));
+        int basW = (int) (xBwidth - 2 * traHeight - mTextBounds.width()) / 2;
+
+        float topTitleBounds = (int) Math.sqrt(Math.pow(basH, 2) + Math.pow(basW, 2));
+
+        topTitleBounds += Math.sqrt(0.5) * mTextBounds.height();
+        return topTitleBounds;
+    }
+
+    private void drawText(Canvas canvas, String text, float x, float y, Paint paint, float angle) {
+        if (angle != 0) {
+            canvas.rotate(angle, x, y);
+        }
+        canvas.drawText(text, x, y, paint);
+        if (angle != 0) {
+            canvas.rotate(-angle, x, y);
         }
     }
 }
